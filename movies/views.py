a5043d009add09cd40c48efd4ef808c62e5e4909
AhmedMIH <m_rx_22@yahoo.com>
2021-01-04 21:26:42 +0200

    final

---
+++ movies/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
+
+def index(request):
+    movies_list = Movie.objects.all()
+    oneMovie = Movie.objects.get(id=1)
+    context = {'movies':movies_list,'oneMovie':oneMovie}
+    return render(request,'movies/index.html',context)
+
+def movieDetails(request,pk):
+    movie_one = Movie.objects.get(id=pk)
+    total = movie_one.total_ticket
+    ava = movie_one.available_ticket
+
+    if request.method =='POST':
+        ava = ava - 1
+        if ava <= total:
+            movie_one = Movie.objects.filter(id=pk).update(available_ticket=ava)
+            return redirect('/')
+
+    context = {'movie':movie_one}
+    return render(request,'movies/details.html',context)
